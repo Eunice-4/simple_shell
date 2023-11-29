3924f9c6c1edb02230f7b59fd0e3df1f6717acfe
Tosinkuzzy <Adeleye608@gmail.com>
2023-11-29 16:54:43 +0100

    parser file

---
+++ parser.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * split_line - Splits a line into tokens
+ * @line: Line to split
+ * Return: Null-terminated array of tokens
+ */
+char **split_line(char *line)
+{
+    /* Tokenize the line */
+    /* Return the tokens */
+}
+
