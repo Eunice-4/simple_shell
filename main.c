782c7df0c5da5ffd8aec383706eaa56775287b5a
Tosinkuzzy <Adeleye608@gmail.com>
2023-11-29 16:52:20 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+    /* Initialize shell */
+    /* Enter shell loop */
+    /* Cleanup and exit */
+    return (0);
+}
+
